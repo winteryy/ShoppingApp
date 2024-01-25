5a295ea4f088005f05815cd10f905d1fef14d07d
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2024-01-25 20:15:03 +0900

    top search bar

--- presentation/src/main/java/com/winterry/presentation/ui/MainScreen.kt
+++ presentation/src/main/java/com/winterry/presentation/ui/MainScreen.kt
@@ -3,18 +3,28 @@ package com.winterry.presentation.ui
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -22,11 +32,12 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.winterry.presentation.R
 import com.winterry.presentation.ui.theme.ShoppingAppTheme
+import com.winterry.presentation.viewmodel.MainViewModel
 
-sealed class MainNavigationItem(var route: String, var name: String) {
-    object Main: MainNavigationItem("Main", "Main")
-    object Category: MainNavigationItem("Category", "Category")
-    object MyPage: MainNavigationItem("MyPage", "MyPage")
+sealed class MainNavigationItem(val route: String, val icon: ImageVector, val name: String) {
+    object Main : MainNavigationItem("Main", Icons.Filled.Home, "Main")
+    object Category : MainNavigationItem("Category", Icons.Filled.Star, "Category")
+    object MyPage : MainNavigationItem("MyPage", Icons.Filled.AccountBox, "MyPage")
 }
 
 @Preview(showBackground = true)
@@ -39,9 +50,13 @@ fun GreetingPreview() {
 
 @Composable
 fun MainScreen() {
+    val viewModel = hiltViewModel<MainViewModel>()
     val navController = rememberNavController()
 
     Scaffold(
+        topBar = {
+            Header(viewModel)
+        },
         bottomBar = {
             MainBottomNavigationBar(navController)
         }
@@ -54,6 +69,21 @@ fun MainScreen() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Header(viewModel: MainViewModel) {
+    TopAppBar(
+        title = { Text("My App") },
+        actions = {
+            IconButton(onClick = {
+                viewModel.openSearchForm()
+            }) {
+                Icon(Icons.Filled.Search, "SearchIcon")
+            }
+        }
+    )
+}
+
 @Composable
 fun MainBottomNavigationBar(navController: NavHostController) {
     val bottomNavigationItems = listOf(
@@ -62,10 +92,7 @@ fun MainBottomNavigationBar(navController: NavHostController) {
         MainNavigationItem.MyPage,
     )
 
-    NavigationBar(
-        containerColor = Color(0xffff0000),
-        contentColor = Color(0xff00ff00)
-    ) {
+    NavigationBar {
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
 
@@ -83,7 +110,7 @@ fun MainBottomNavigationBar(navController: NavHostController) {
                         restoreState = true
                     }
                 },
-                icon = { Icon(painterResource(id = R.drawable.ic_launcher_foreground), item.route) })
+                icon = { Icon(item.icon, item.route) })
         }
     }
 }
