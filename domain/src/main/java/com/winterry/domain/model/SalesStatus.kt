2c233984b77da983886a9abb2f2ecfcc39e7d14c
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2024-01-26 02:45:35 +0900

    common Product Card Composable

--- domain/src/main/java/com/winterry/domain/model/SalesStatus.kt
+++ domain/src/main/java/com/winterry/domain/model/SalesStatus.kt
@@ -2,6 +2,6 @@ package com.winterry.domain.model
 
 enum class SalesStatus {
     ON_SALE,
-    NORMAL,
+    ON_DISCOUNT,
     SOLD_OUT
 }
---
+++ presentation/src/main/java/com/winterry/presentation/ui/common/ProductCard.kt
@@ -0,0 +1,171 @@
+package com.winterry.presentation.ui.common
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.winterry.domain.model.Category
+import com.winterry.domain.model.Product
+import com.winterry.domain.model.SalesStatus
+import com.winterry.domain.model.Shop
+import com.winterry.presentation.R
+import com.winterry.presentation.ui.theme.Purple40
+import com.winterry.presentation.ui.theme.Purple80
+
+@Composable
+fun ProductCard(product: Product, onClick: (Product) -> Unit?) {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(intrinsicSize = IntrinsicSize.Max)
+            .padding(10.dp)
+            .shadow(elevation = 10.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(320.dp)
+                .padding(10.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.product_image),
+                contentDescription = "product image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentWidth(Alignment.End)
+            )
+            Text(
+                fontSize = 14.sp,
+                fontWeight = FontWeight.SemiBold,
+                text = product.shop.shopName
+            )
+            Text(
+                fontSize = 14.sp,
+                text = product.productName
+            )
+            Price(product = product)
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun PreviewProductCard() {
+    ProductCard(
+        product = Product(
+            productId = "1",
+            productName = "상품 이름",
+            imageUrl = "",
+            price = com.winterry.domain.model.Price(30000, 30000, SalesStatus.ON_SALE),
+            category = Category.Top,
+            shop = Shop("1", "샵 이름", ""),
+            isNew = false,
+            isFreeShipping = false
+        )
+    ) {
+
+    }
+}
+
+@Composable
+@Preview
+private fun PreviewProductCardDiscount() {
+    ProductCard(
+        product = Product(
+            productId = "1",
+            productName = "상품 이름",
+            imageUrl = "",
+            price = com.winterry.domain.model.Price(30000, 20000, SalesStatus.ON_DISCOUNT),
+            category = Category.Top,
+            shop = Shop("1", "샵 이름", ""),
+            isNew = false,
+            isFreeShipping = false
+        )
+    ) {
+
+    }
+}
+
+@Composable
+@Preview
+private fun PreviewProductCardSoldOut() {
+    ProductCard(
+        product = Product(
+            productId = "1",
+            productName = "상품 이름",
+            imageUrl = "",
+            price = com.winterry.domain.model.Price(30000, 30000, SalesStatus.SOLD_OUT),
+            category = Category.Top,
+            shop = Shop("1", "샵 이름", ""),
+            isNew = false,
+            isFreeShipping = false
+        )
+    ) {
+
+    }
+}
+
+@Composable
+private fun Price(product: Product) {
+    when (product.price.salesStatus) {
+        SalesStatus.ON_SALE -> {
+            Text(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                text = "${product.price.originPrice}"
+            )
+        }
+
+        SalesStatus.ON_DISCOUNT -> {
+            Text(
+                fontSize = 14.sp,
+                text = "${product.price.originPrice}",
+                textDecoration = TextDecoration.LineThrough
+            )
+            Row {
+                Text(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    text = "할인가: "
+                )
+                Text(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Purple40,
+                    text = "${product.price.finalPrice}"
+                )
+            }
+        }
+
+        SalesStatus.SOLD_OUT -> {
+            Text(
+                fontSize = 18.sp,
+                color = Color(0xFF666666),
+                text = "판매종료"
+            )
+        }
+    }
+}
